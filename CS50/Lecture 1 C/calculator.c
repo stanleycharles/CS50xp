4e3a7fe74fb4c323a20a544605cabbb40e7375f3
Stan <stanleycharles@live.fr>
2022-10-08 16:19:43 +0200

    Add files via upload

---
+++ CS50/Lecture 1 C/calculator.c
@@ -0,0 +1,14 @@
+#include <cs50.h> // not working
+#include <stdio.h>
+
+int main(void)
+{
+  // Prompt user for x
+  int x = get_int("x: ");
+
+  // Prompt user for y
+  int y = get_int("y: ");
+
+  // Prompt addition
+  printf("%i\n", x + y);m  
+}
---
+++ CS50/Lecture 1 C/hello.c
@@ -0,0 +1,8 @@
+#include <cs50.h> // not working
+#include <stdio.h>
+
+int main(void)
+{
+    string answer = get_string("What's your Name? ");
+    printf("hello, %s\n", answer);
+} 
